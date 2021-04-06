83096a786ff945217ef49c3e0ee3d7f1fb825505
MichaelMcFaddenjr <michaelpmcfaddenjr@gmail.com>
2021-04-06 14:26:08 -0600

    completed mvp

--- js/index.js
+++ js/index.js
@@ -1,1 +1,52 @@
 // Your code goes here
+
+// mouseover
+
+const logoHeading = document.querySelector('.logo-heading');
+logoHeading.addEventListener("mouseover", function (event){
+    event.target.style.color = "red";
+    setTimeout(function(){
+        event.target.style.color = "";
+    }, 1000);
+}, false);
+
+const introHeader = document.querySelector('.intro');
+introHeader.addEventListener('mouseover', function(event){
+    event.target.style.color = "blue";
+    setTimeout(function(){
+        event.target.style.color = "";
+    }, 1000);
+}, false);
+
+// resize
+
+window.addEventListener("resize", function(){
+    alert("Resized window!")
+})
+
+// load
+
+window.addEventListener("load", function(){
+    alert("Time to Travel")
+})
+
+// scroll
+
+const navContainer = document.querySelector("body")
+window.addEventListener("scroll", function(event){
+    navContainer.style.background = "lightpink";
+    setTimeout(function(){
+        navContainer.style.background = "";
+    }, 1000);
+}, false);
+
+// click
+
+const button = document.querySelector(".btn1");
+
+function buttonClick() {
+    button.textContent = (`You Clicked Me`);
+}
+
+button.addEventListener("click", buttonClick);
+
